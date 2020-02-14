a5788a229004c00324807b3d7b84a9f4a19b3009
Darkchaos2 <louis_james1998@hotmail.co.uk>
2020-02-14 00:49:27 +0000

    Initial commit

---
+++ graphics/smash.js
@@ -0,0 +1,74 @@
+var open = false;
+
+function update(player1, player2) {
+	closeWings(
+		function() {
+			$('#leftName').html(player1);
+			$('#leftTextBox').textfill();
+		},
+		function() {
+			$('#rightName').html(player2);
+			$('#rightTextBox').textfill();
+		}
+	);
+
+	setTimeout(function(){
+		openWings();
+	}, 2500);
+}
+
+function openWings() {
+	$("#leftWing").animate(
+		{
+			left: '0'
+		},
+		750
+	);
+
+	$("#rightWing").animate(
+		{
+			right: '0'
+		},
+		750
+	);
+
+	open = true;
+}
+
+function closeWings(onLeftFinishAction = function() {}, onRightFinishAction = function() {}) {
+	$("#leftWing").animate(
+		{
+			left: '268px'
+		},
+		750,
+		() => {
+			onLeftFinishAction();
+		}
+	);
+
+	$("#rightWing").animate(
+		{
+			right: '268px'
+		},
+		750,
+		() => {
+			onRightFinishAction();
+		}
+	);
+
+	open = false;
+}
+
+const nameReplicant = nodecg.Replicant('playerNames', '0_dashboards');
+
+nameReplicant.on('change', (newValue, oldValue) => {
+	update(newValue[0], newValue[1]);
+});
+
+nodecg.listenFor("openWings", message => {
+	openWings();
+});
+
+nodecg.listenFor("closeWings", message => {
+	closeWings();
+});
